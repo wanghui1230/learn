752240396671bd60da31f4e7d760b0db7c9f25b8
wanghui <919788368@qq.com>
2023-03-08 22:41:39 +0800

    我的项目

---
+++ luck/src/main/java/com/good/luck/LuckApplication.java
@@ -0,0 +1,13 @@
+package com.good.luck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuckApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuckApplication.class, args);
+    }
+
+}
---
+++ luck/src/test/java/com/good/luck/LuckApplicationTests.java
@@ -0,0 +1,13 @@
+package com.good.luck;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuckApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
